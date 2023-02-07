a58ab882d83101e7452798a177338f966797d808
sagarkakkar03 <122864251+sagarkakkar03@users.noreply.github.com>
2023-02-07 11:24:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,6 +60,7 @@ if uploaded_file is not None:
     try:
         user_list.remove('group_notification')
     except:
+        pass
     user_list.sort()
     user_list.insert(0, 'Overall')
     selected_user = st.sidebar.selectbox('show analysis wrt', user_list)
